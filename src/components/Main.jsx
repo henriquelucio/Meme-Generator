c313f1a837814e9304cae46e63fd1b32674879cf
Henrique <henriquelucio95@hotmail.com>
2026-04-03 20:22:03 -0300

    Update meme text content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,6 +8,14 @@ export default function Main(){
         imageUrl:"http://i.imgflip.com/1bij.jpg"
     })
 
+    function handleChange(event){
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme, 
+            [name]: value
+        }))
+    }
+
     return (
         <main>
             <div className="form">
@@ -16,6 +24,8 @@ export default function Main(){
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
                     />
                 </label>
                 <label>Botton Text
@@ -23,6 +33,8 @@ export default function Main(){
                         type="text"
                         placeholder="Walk into mordor"
                         name="bottonText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
